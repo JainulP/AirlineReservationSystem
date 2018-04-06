8fd2d81cc671877db14a9f0e1ab70417616e666f
manasacsy <32827245+manasacsy@users.noreply.github.com>
2018-04-05 22:42:31 -0700

    controllers code

--- AirlineReservationSystem/src/main/java/sjsu/cmpe275/lab2/controller/FlightController.java
+++ AirlineReservationSystem/src/main/java/sjsu/cmpe275/lab2/controller/FlightController.java
@@ -1,8 +1,10 @@
 package sjsu.cmpe275.lab2.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/flight")
 public class FlightController {
 
 }
--- AirlineReservationSystem/src/main/java/sjsu/cmpe275/lab2/controller/PassengerController.java
+++ AirlineReservationSystem/src/main/java/sjsu/cmpe275/lab2/controller/PassengerController.java
@@ -1,8 +1,29 @@
 package sjsu.cmpe275.lab2.controller;
 
+import javax.websocket.server.PathParam;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PassengerController {
+import sjsu.cmpe275.lab2.model.Greeting;
 
+@RestController
+@RequestMapping("/passenger/{id}")
+public class PassengerController {
+	@SuppressWarnings("null")
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity getPassengerJson(@PathVariable("id") Long id) throws JSONException {
+		JSONObject temp = new JSONObject();
+		System.out.println(id);
+		temp.put("value", id);
+		return new ResponseEntity(temp.toString(), HttpStatus.CREATED);
+	}
 }
