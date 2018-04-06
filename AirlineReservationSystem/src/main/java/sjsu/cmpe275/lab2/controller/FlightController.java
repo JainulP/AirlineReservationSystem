2afcabbac82b3f12e0215d1f362f6c5dac7bd892
manasacsy <32827245+manasacsy@users.noreply.github.com>
2018-04-05 18:30:45 -0700

    added controllers

---
+++ AirlineReservationSystem/src/main/java/sjsu/cmpe275/lab2/controller/FlightController.java
@@ -0,0 +1,8 @@
+package sjsu.cmpe275.lab2.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlightController {
+
+}
--- AirlineReservationSystem/src/main/java/sjsu/cmpe275/lab2/controller/GreetingController.java
+++ AirlineReservationSystem/src/main/java/sjsu/cmpe275/lab2/controller/GreetingController.java
@@ -1,4 +1,5 @@
 package sjsu.cmpe275.lab2.controller;
+
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,13 +9,12 @@ import sjsu.cmpe275.lab2.model.Greeting;
 
 @RestController
 public class GreetingController {
-	
-	private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 }
